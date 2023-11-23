3c6006618c4b325bc413a450b1357231bd9f529b
Tothvillar <149988497+Tothvillar@users.noreply.github.com>
2023-11-23 21:34:03 +0800

    Add files via upload

---
+++ 女朋友.java
@@ -0,0 +1,10 @@
+public class 女朋友 {
+    String name;
+    int age;
+    public void playGame(){
+        System.out.println("女朋友在打游戏");
+    }
+    public void slpeep(){
+        System.out.println("女朋友在睡觉");
+    }
+}
